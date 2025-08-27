1332d49418119932794c92754dd424460074ce18
Alexander Johnson <alexjohnson0123@gmail.com>
2025-08-27 12:19:16 -0400

    added my name

--- index.php
+++ index.php
@@ -123,6 +123,7 @@ if (isset($_SESSION['username'])) {
       </table>
     </div>
   </div>
+  <p>Created by Alexander Johnson</p>
   <!---------------->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <script src="./popcharts.js"></script>
